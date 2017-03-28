320a603784caf429265619c5c08b4731fc3c6353
root <root@packer-debian-8-amd64.droplet.local>
2017-03-28 19:21:30 +0000

    salvador`s db script

---
+++ db-setup.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `users` (
+ `user_id` int(11) NOT NULL AUTO_INCREMENT,
+ `firstName` varchar(255) DEFAULT NULL,
+ `lastName` varchar(255) DEFAULT NULL,
+ `username` varchar(255) DEFAULT NULL,
+ `password` varchar(255) DEFAULT NULL,
+ `email` varchar(255) DEFAULT NULL,
+ `active` int(11) DEFAULT NULL,
+ `join_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ `avatar_url` mediumtext,
+ PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+CREATE TABLE logs (
+  `logs_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` int,
+  `image` varchar(8000),
+  `date_taken` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(logs_id),
+  FOREIGN KEY(user) REFERENCES users(user_id)
+);
